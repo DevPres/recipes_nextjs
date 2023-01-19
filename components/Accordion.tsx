a32b7f4002ff206262065e9117ca4fde25663c4f
Dario <dariopresdev@protonmail.com>
2023-01-19 17:34:18 +0100

    less text for accordion

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 
 
-export default function Accordion({children, open, id, title}: {children: React.ReactNode, open: Boolean, id:string, title:string}) {
+export default function Accordion({children, open, id, title}: {children?: React.ReactNode, open: Boolean, id:string, title:string}) {
   const [isOpen, setOpen] = useState(open)
   return (
     <div  className="m-0 p-0 border bg-alt-50  w-full">
-      <h3 className="m-0 p-0">
+      <h3 className="m-0 p-0 text-md">
         <button id={`controlof-${id}`} type="button" aria-expanded={isOpen ? "true" : "false"} className="bg-transparent m-0 py-4 px-6 w-full text-left	" aria-controls={id} onClick={() => setOpen(!isOpen)}>
           <span className="accordion-title text-primary">
             {title}
