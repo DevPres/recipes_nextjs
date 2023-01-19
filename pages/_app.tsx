a8b96d571066900f5fd67a66415c44610508661d
Dario <dariopresdev@protonmail.com>
2023-01-19 17:34:46 +0100

    demo purpose

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import '@/styles/globals.css'
 import Layout from '@/components/Layout/Layout'
 import type { AppProps } from 'next/app'
-import { Merriweather_Sans } from '@next/font/google'
+import { Source_Code_Pro } from '@next/font/google'
+import { Hahmlet } from '@next/font/google'
 
-const sourcecodepro = Merriweather_Sans({
+const sourcecodepro = Source_Code_Pro({
   subsets: ['latin'],
   variable: '--font-sourcecodepro',
+})
+const hamlet = Hahmlet({
+  subsets: ['latin'],
+  variable: '--font-hamlet',
 })
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <div className={`${sourcecodepro.variable} font-sans bg-alt-100 min-h-screen`}>
+    <div className={`${sourcecodepro.variable} ${hamlet.variable} font-body text-base bg-primary-300 min-h-screen`}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
