94712dbfdcf36171e401cbc53adff93228bdf9ca
Dario <dariopresdev@protonmail.com>
2023-01-19 17:33:27 +0100

    added font size and color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,19 +38,61 @@ module.exports = {
       },
       error: '#E81221',
       success: '#81E052',
-      info: '#FBC850'
+      warn: '#FBC850'
     },
-    
+    fontSize: {
+      base: 'var(--text-base-size)',
+      xs: 'var(--text-xs)',
+      sm: 'var(--text-sm)',
+      md: 'var(--text-md)',
+      lg: 'var(--text-lg)',
+      xlg: 'var(--text-xlg)',
+      '2xlg': 'var(--text-2xlg)',
+      '3xlg': 'var(--text-3xlg)',
+      '4xlg': 'var(--text-4xlg)',
+      '5xlg': 'var(--text-5xlg)',
+      '6xlg': 'var(--text-6xlg)',
+      '7xlg': 'var(--text-7xlg)',
+      '8xlg': 'var(--text-8xlg)',
+      '9xlg': 'var(--text-9xlg)',
+    },
     extend: {
       textColor: ({ theme }) => ({
-        white: 'white',
-        black: 'black',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        'gray-1': 'var(--gray-1)',
+        'gray-2': 'var(--gray-2)',
+        'gray-3': 'var(--gray-3)',
+        'gray-4': 'var(--gray-4)',
+        'gray-5': 'var(--gray-5)',
+        'gray-6': 'var(--gray-6)',
+        'gray-7': 'var(--gray-7)',
         primary: theme('colors.primary[900]'),
         alt: theme('colors.alt[900]')
       }),
       fontFamily: {
-        sans: ["var(--font-sourcecodepro)", ...fontFamily.sans]
+        heading: ["var(--font-sourcecodepro)", ...fontFamily.sans],
+        body: ["var(--font-hamlet)", ...fontFamily.serif],
+        button: ["var(--font-sourcecodepro)", ...fontFamily.sans],
       },
+      spacing: {
+        'xxxs': 'var(--space-xxxs)',
+        'xxs': 'var(--space-xxs)',
+        'xs': 'var(--space-xs)',
+        'sm': 'var(--space-sm)',
+        'md': 'var(--space-md)',
+        'lg': 'var(--space-lg)',
+        'xl': 'var(--space-xl)',
+        'xxl': 'var(--space-xxl)',
+        'xxxl': 'var(--space-xxxl)',
+      },
+      maxWidth: {
+        'line-length':'var(--line-length)'
+      },
+      lineHeight: {
+        heading: 'var(--heading-line-height)',
+        body: 'var(--body-line-height)'
+      }
     }
   },
   plugins: [],
