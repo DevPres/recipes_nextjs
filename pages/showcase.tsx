f21f74e998b2b787eabab5ea2d976b4b99bdb601
Dario <dariopresdev@protonmail.com>
2023-01-19 17:35:01 +0100

    augmented show case

--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -8,7 +8,33 @@ export default function ShowCase() {
   }
   return (
     <>
+    <Accordion open={false} id="btns" title="Text">
+    <h1>Heading H1</h1>
+    <h2>Heading H2</h2>
+    <h3>Heading H3</h3>
+    <h4>Heading H4</h4>
+    <h5>Heading H5</h5>
+    <h6>Heading H6</h6>
+    <hr />
+    <p>Many Jimdo Creator templates have full-width content areas (like template Zurich in the example above), which make them ideal for showing large images or photo galleries. When it comes to landing page text, though, it’s best not to let your lines run across the entire width. Try using Columns elements and Photo elements to break up the text and create shorter lines.</p>
+    <hr />
+    <article>
+      <hgroup>
+        <h1>
+          Article heading h1
+        </h1>
+        <p>
+          - Article sub heading h6 thast is loong more than one line
+        </p>
+      </hgroup>
+      <p>
+      Notice how the lowest lightness in the dark text color set is 65% and the highlest lightness in the dark surfaces is 25%. That's 40% of lightness breathing room between them. In the light theme, there's 55% breathing room in the light theme. Keeping lightness differences between text and surface colors at around 40-50% can help keep color contrast ratios high, while also being a subtle lever to adjust in case scores are poor.
+      </p>
 
+    </article>
+
+    
+    </Accordion>
     <Accordion open={false} id="btns" title="Buttons">
       <>
       <div className="flex flex-col gap-x-8 gap-y-4 flex-wrap p-4">
@@ -50,8 +76,6 @@ export default function ShowCase() {
        
 
     </Accordion>
-    
-    
     </>
   )
 }
